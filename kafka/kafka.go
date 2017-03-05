fd2e4fabb16d3ccd0bc3bf2d712bd4d76a26cd3c
Jakob Karalus <jakob.karalus@gmx.net>
2017-03-05 22:19:18 +0100

    Kafka Util Stub

---
+++ kafka/kafka.go
@@ -0,0 +1,33 @@
+package kafka
+
+import (
+	"github.com/Shopify/sarama"
+	"fmt"
+)
+
+type KafkaUtil struct {
+	KafkaClient sarama.Client
+	BrokerList []string
+	ClusterName string
+}
+
+
+func  New(brokerList []string, clusterName string) (*KafkaUtil, error){
+	config := sarama.NewConfig()
+
+	fmt.Println("Creating KafkaUtil for cluster with Brokers: ", clusterName, brokerList )
+
+	client, err := sarama.NewClient(brokerList, config)
+	if err != nil {
+		fmt.Println("Error creating Kafka Client: ", err)
+	}
+
+	k := &KafkaUtil{
+		KafkaClient: client,
+		ClusterName: clusterName,
+		BrokerList: brokerList,
+	}
+
+	fmt.Println("Initilized Kafka CLient")
+	return k, nil
+}
