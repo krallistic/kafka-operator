e053c028821e879072b57dc93a6e3829c6690920
Jakob Karalus <jakob.karalus@gmx.net>
2017-09-01 12:00:16 +0200

    Added 02-test

--- e2e-tests/run-tests.sh
+++ e2e-tests/run-tests.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-
+echo "Setting Up Cluster"
 source hack/setup-gcloud-cluster.sh
 
+echo "Running BATS Tests"
 bats 01-test-basic-setup.bats
+bats 02-test-kafka-setup.bats
 
+echo "Destroying Cluster"
 source hack/delete-gcloud-cluser.sh
