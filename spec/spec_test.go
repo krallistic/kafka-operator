98aad7cb1ff40e9b795ad7dcf542cd706a18dfcc
Jakob Karalus <jakob.karalus@gmx.net>
2017-12-08 13:13:35 +0100

    Commented out fuzzing test, should move to code_gen soon

--- spec/spec_test.go
+++ spec/spec_test.go
@@ -1,18 +1,8 @@
 package spec
 
 import (
-	"math/rand"
-	"testing"
-
-	"github.com/google/gofuzz"
-
-	apitesting "k8s.io/apimachinery/pkg/api/testing"
-	fuzzer "k8s.io/apimachinery/pkg/api/testing/fuzzer"
-	roundtrip "k8s.io/apimachinery/pkg/api/testing/roundtrip"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
 )
 
 var _ runtime.Object = &Kafkacluster{}
@@ -21,30 +11,30 @@ var _ metav1.ObjectMetaAccessor = &Kafkacluster{}
 var _ runtime.Object = &KafkaclusterList{}
 var _ metav1.ListMetaAccessor = &KafkaclusterList{}
 
-func exampleFuzzerFuncs(t apitesting.TestingCommon) []interface{} {
-	return []interface{}{
-		func(obj *KafkaclusterList, c fuzz.Continue) {
-			c.FuzzNoCustom(obj)
-			obj.Items = make([]Kafkacluster, c.Intn(10))
-			for i := range obj.Items {
-				c.Fuzz(&obj.Items[i])
-			}
-		},
-	}
-}
+// func exampleFuzzerFuncs(t apitesting.TestingCommon) []interface{} {
+// 	return []interface{}{
+// 		func(obj *KafkaclusterList, c fuzz.Continue) {
+// 			c.FuzzNoCustom(obj)
+// 			obj.Items = make([]Kafkacluster, c.Intn(10))
+// 			for i := range obj.Items {
+// 				c.Fuzz(&obj.Items[i])
+// 			}
+// 		},
+// 	}
+// }
 
-// TestRoundTrip tests that the third-party kinds can be marshaled and unmarshaled correctly to/from JSON
-// without the loss of information. Moreover, deep copy is tested.
-func TestRoundTrip(t *testing.T) {
-	scheme := runtime.NewScheme()
-	codecs := serializer.NewCodecFactory(scheme)
+// // TestRoundTrip tests that the third-party kinds can be marshaled and unmarshaled correctly to/from JSON
+// // without the loss of information. Moreover, deep copy is tested.
+// func TestRoundTrip(t *testing.T) {
+// 	scheme := runtime.NewScheme()
+// 	codecs := serializer.NewCodecFactory(scheme)
 
-	AddToScheme(scheme)
+// 	AddToScheme(scheme)
 
-	seed := rand.Int63()
-	fuzzerFuncs := fuzzer.MergeFuzzerFuncs(t, fuzzer.GenericFuzzerFuncs(t, codecs), exampleFuzzerFuncs(t))
-	fuzzer := fuzzer.FuzzerFor(fuzzerFuncs, rand.NewSource(seed))
+// 	seed := rand.Int63()
+// 	fuzzerFuncs := fuzzer.MergeFuzzerFuncs(t, fuzzer.GenericFuzzerFuncs(t, codecs), exampleFuzzerFuncs(t))
+// 	fuzzer := fuzzer.FuzzerFor(fuzzerFuncs, rand.NewSource(seed))
 
-	roundtrip.RoundTripSpecificKindWithoutProtobuf(t, SchemeGroupVersion.WithKind("Kafkacluster"), scheme, codecs, fuzzer, nil)
-	roundtrip.RoundTripSpecificKindWithoutProtobuf(t, SchemeGroupVersion.WithKind("KafkaclusterList"), scheme, codecs, fuzzer, nil)
-}
+// 	roundtrip.RoundTripSpecificKindWithoutProtobuf(t, SchemeGroupVersion.WithKind("Kafkacluster"), scheme, codecs, fuzzer, nil)
+// 	roundtrip.RoundTripSpecificKindWithoutProtobuf(t, SchemeGroupVersion.WithKind("KafkaclusterList"), scheme, codecs, fuzzer, nil)
+// }
